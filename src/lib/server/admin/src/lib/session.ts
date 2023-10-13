19255e530c2dafbe57cd01cfcbb079ac8e5e78e7
kotekpsotek <siematumichael@protonmail.com>
2023-10-13 23:41:12 +0200

    Session handle module for admin app

---
+++ src/lib/server/admin/src/lib/session.ts
@@ -0,0 +1,83 @@
+import { randomUUID } from "crypto";
+import type { Cookies } from "@sveltejs/kit";
+import * as mongodb from "../../../databases/mogodb";
+import { Schema } from "mongoose";
+
+interface AdminCookie {
+    sess_id: string,
+    date_set?: Date
+    expiration_date: Date
+}
+
+const sessionAdminSchema = new Schema<AdminCookie>({
+    sess_id: { type: String, required: true },
+    date_set: { type: Date, default: () => new Date() },
+    expiration_date: { type: Date, required: true }
+})
+const sessionAdminCookiesModel = mongodb.connection.model("admin_sessions", sessionAdminSchema);
+
+export class SessionDestroy {
+    static async destroy(sessionId: string) {
+
+    }
+}
+
+export class SessionWrite extends SessionDestroy {
+    constructor(cookies: Cookies, locals: App.Locals) {
+        const sessionId = randomUUID();
+        const expDate = new Date(Date.now() + (24 * 60 * 60 * 1_000));
+        
+        // Save in databse
+        sessionAdminCookiesModel.create({
+            sess_id: sessionId,
+            expiration_date: expDate
+        })
+
+        // For client
+        cookies.set("sess", sessionId, {
+            expires: expDate,
+            path: "/",
+            httpOnly: true,
+        });
+
+        // For app routing
+        locals.login = true;
+
+        // Initialize extend classes
+        super();
+    }
+}
+
+export class SessionRead {
+    constructor(cookies: Cookies, locals: App.Locals) {
+        const cookieSes = cookies.get("sess");
+
+        if (cookieSes) {
+            // Check in databse
+            sessionAdminCookiesModel.exists({ 
+                sess_id: cookieSes
+            }).then(async res => {
+                if (res) {
+                    locals.login = true
+
+                    // Update session expiration data
+                    const expDate = new Date(Date.now() + (24 * 60 * 60 * 1_000));
+                        // ...Databse
+                    await sessionAdminCookiesModel.findOneAndUpdate({
+                        sess_id: cookieSes
+                    }, {
+                        expiration_date: expDate,
+                        date_set: new Date()
+                    });
+                        // ...Client side
+                    cookies.set("sess", cookieSes, {
+                        expires: expDate,
+                        path: "/",
+                        httpOnly: true
+                    });
+                }
+            })
+            
+        }
+    }
+}
