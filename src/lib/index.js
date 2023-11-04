6aa9bef0a0a49a85664564c7d8926c862deca3b6
kotekpsotek <siematumichael@protonmail.com>
2023-11-04 21:17:41 +0100

    In shape, project now is ready to use via users

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,10 @@
 // Reexport your entry components here
+import { chatPlugin as svelteChatPlugin, load, loadLayoutServer } from "./server/as-plugin.js";
+import SvelteChatButton from "./client/Layout.svelte";
+
+export {
+    load,
+    loadLayoutServer,
+    svelteChatPlugin,
+    SvelteChatButton
+}
